cf984cfad51c7c0268114a1fd7b794a2b1d647df
AryanS15 <47918798+AryanS15@users.noreply.github.com>
2020-04-24 02:15:22 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ window.$ = $;
 global.jQuery = $;
 
 const DashboardDefault = React.lazy(() => import('./Demo/Dashboard/Default'));
-const Nvd3Chart = React.lazy(() => import('./Demo/Charts/Nvd3Chart/index'));
+//const Nvd3Chart = React.lazy(() => import('./Demo/Charts/Nvd3Chart/index'));
 const routes = [
     { path: '/dashboard/default', exact: true, name: 'Default', component: DashboardDefault },
-    { path: '/charts/nvd3', exact: true, name: 'Nvd3 Chart', component: Nvd3Chart },
+  //  { path: '/charts/nvd3', exact: true, name: 'Nvd3 Chart', component: Nvd3Chart },
 ];
 
 export default routes;
